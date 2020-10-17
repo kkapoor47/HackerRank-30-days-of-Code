1e170e8acdfaafafda9499d4bb4995631c901cf2
Khushi Kapoor <70375438+kkapoor1394@users.noreply.github.com>
2020-10-17 19:33:56 +0530

    Add files via upload

---
+++ Day-9 Recursion 3/day_9.py
@@ -0,0 +1,56 @@
+'''
+Day 9: Recursion 3
+
+
+Objective
+Today, we’re learning and practicing an algorithmic concept called Recursion. Check out the Tutorial tab for learning materials and an instructional video!
+
+Recursive Method for Calculating Factorial
+
+Task
+Write a factorial function that takes a positive integer, as a parameter and prints the result of ( factorial).
+
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of .
+
+Input Format
+
+A single integer, (the argument to pass to factorial).
+
+Constraints
+
+Your submission must contain a recursive function named factorial.
+Output Format
+
+Print a single integer denoting .
+
+Sample Input
+
+3
+Sample Output
+
+6
+'''
+# Answer
+import math
+import os
+import random
+import re
+import sys
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n * factorial(n-1)
+       
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
